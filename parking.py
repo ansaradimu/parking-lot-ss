51ee55602c09ae004af809d0991127f2210f8668
Ansar K.A <ansar.ka@byjus.com>
2021-02-27 21:22:56 +0530

    + Init commit. Added skelton classes

---
+++ parking.py
@@ -0,0 +1,35 @@
+
+
+class Car:
+	def __init__(self,reg_num,driver_age):
+		self.reg_num = reg_num
+		self.driver_age = driver_age
+		
+		
+class ParkingSystem:
+	
+	def __init__(self):
+		pass
+		
+	def create_parking_lots(self):
+		pass
+		
+	def get_cars_by_driver_age(self,driver_age):
+		pass
+	
+	def get_slot_id_by_regno(self,reg_num):
+		pass
+		
+	def get_slots_by_driver_age(self,driver_age):
+		pass
+		
+	def park_a_car(self,car):
+		pass
+		
+	def unpark_a_park(self,car):
+		pass
+		
+		
+		
+		
+
