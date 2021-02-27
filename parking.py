7e15299b892a49ae7ec1c06a46bcd7aeb399215f
Ansar K.A <ansar.ka@byjus.com>
2021-02-28 00:22:43 +0530

    + Working code for car parking problem

--- parking.py
+++ parking.py
@@ -2,6 +2,9 @@ import heapq
 from collections import defaultdict
 
 class Car:
+	'''
+		CAR class with 2 properties. Vehichle number (reg_num) and driver's age (driver_age)
+	'''
 	def __init__(self,reg_num,driver_age):
 		self.reg_num = reg_num
 		self.driver_age = driver_age
@@ -9,20 +12,29 @@ class Car:
 		
 class ParkingSystem:
 	
+	'''
+		ParkingSystem class. All the parking related activitis will be controlled from here
+	'''
+	
 	def __init__(self):
 		self.available_slot_list = []
 		
-		self.slot_car_map = dict() # slot -> Car details
-		self.age_car_map = defaultdict(list) # 
+		self.slot_car_map = dict() 
+		self.age_car_map = defaultdict(list)
 		self.age_slot_map = defaultdict(list) 
 		
 	def create_parking_lots(self,total_slots):
 		
 		for slot in range (1, total_slots+1):
 			heapq.heappush(self.available_slot_list,slot)
+			
+		print("Created parking of {} slots".format(total_slots))
 		
 	def get_cars_by_driver_age(self,driver_age):
-		print ("Cars by age::",self.age_car_map[driver_age])
+		if len(self.age_car_map[driver_age]) == 0:
+			print ("None")
+			return
+		print (self.age_car_map[driver_age])
 	
 	def get_slot_id_by_regno(self,reg_num):
 		slot_det = None
@@ -31,23 +43,22 @@ class ParkingSystem:
 				slot_det=slot
 				break
 				
-		print ("Slot::",slot_det)
+		print (slot_det)
 		pass
 		
 	def get_slots_by_driver_age(self,driver_age):
-		print ("Slots by age::",self.age_slot_map[driver_age])
-		pass
+		if (len(self.age_slot_map[driver_age]) == 0):
+			print ("None")
+			return
+		print (self.age_slot_map[driver_age])
 		
 	def park_a_car(self,car):
 		if self.available_slot_list:
 			available_slot = heapq.heappop(self.available_slot_list)
-			print ("Available slot ::",available_slot)
 			self.slot_car_map[available_slot] = car
 			self.age_car_map[car.driver_age].append(car.reg_num)
 			self.age_slot_map[car.driver_age].append(available_slot)
-			
-			#print ("Age -> Car",self.age_car_map)
-			#print ("Age -> Slot",self.age_slot_map)
+			print ("Car with vehicle registration number \"{}\" has been parked at slot number {}".format(car.reg_num,available_slot))
 		else:
 			print ("Sorry! Slots are full")
 			
@@ -56,29 +67,13 @@ class ParkingSystem:
 	def unpark_a_park(self,slot_num):
 		if self.slot_car_map.has_key(slot_num):
 			car_det = self.slot_car_map[slot_num]
-			print ("Yes",car_det.reg_num)
 			del self.slot_car_map[slot_num]
 			self.age_car_map[car_det.driver_age].remove(car_det.reg_num)
 			self.age_slot_map[car_det.driver_age].remove(slot_num)
 			heapq.heappush(self.available_slot_list, slot_num)
+			print ("Slot number {} vacated, the car with vehicle registration number \"{}\" left the space, the driver of the car was of age {}".format(slot_num,car_det.reg_num,car_det.driver_age))
 		else:
-			print ("No")
+			print ("Slot already vacant")
 			
-		
-c1 = Car("3",25)
-c2 = Car("2",25)
-c3 = Car("1",26)
-
-p1 = ParkingSystem()
-p1.create_parking_lots(3)
-
-p1.park_a_car(c1)
-p1.park_a_car(c2)
-p1.park_a_car(c3)
-
-p1.get_slot_id_by_regno("3")
-#p1.unpark_a_park(c2)
-
-		
 		
 
--- parking_input_parser.py
+++ parking_input_parser.py
@@ -4,28 +4,34 @@ from parking import Car, ParkingSystem
 parking_sys = ParkingSystem()
 
 def input_parser(command):
-	print (command)
+	#print (command)
 	if len(command.strip()) > 0:
 		command_split = command.split()
-		print(command_split)
+		#print(command_split)
 		
 		main_command = command_split[0].strip().lower()
 		
 		if main_command == "create_parking_lot":
 			parking_sys.create_parking_lots(int(command_split[1].strip()))
-			pass
 			
 		elif main_command == "park":
-			pass
+			car = Car(command_split[1].strip(),command_split[3].strip())
+			parking_sys.park_a_car(car);
 			
 		elif main_command == "slot_numbers_for_driver_of_age":
-			pass
+			parking_sys.get_slots_by_driver_age(command_split[1].strip());
 			
 		elif main_command == "leave":
-			pass
+			parking_sys.unpark_a_park(int(command_split[1].strip()))
 			
 		elif main_command == "vehicle_registration_number_for_driver_of_age":
-			pass
+			parking_sys.get_cars_by_driver_age(command_split[1].strip())
+			
+		elif main_command == "slot_number_for_car_with_number":
+			parking_sys.get_slot_id_by_regno(command_split[1].strip())
+			
+		else:
+			print ("Wrong command found, check the command file and retry..")
 
 
 if len(sys.argv) < 2:
